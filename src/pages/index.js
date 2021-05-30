a3005bf97223093a388540e51e7670caa38d8dc2
Marko Mladenovic <marko.mladenovic988@gmail.com>
2021-05-30 15:32:07 +0200

    Finished Gatsby Tutorial Part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="HomePage">
-      <p>I'm making this by following the Gatsby Tutorial.</p>  
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage 
+        alt="Cute foxes"
+        src="../images/jeremy-hynes-BU82EbpT7hk-unsplash.jpg"
+      />  
     </Layout>
   )
 }
